b1a2c34626c532c7802d4d4c759c9d600b179ff3
Snehal979 <bansodsnehal535@gmail.com>
2022-11-20 09:12:04 +0530

    find interger number return first value

---
+++ FindMaxNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxNum
+{
+    public class FindMaxNumber
+    {
+        public int FindMaxInterger(int a, int b, int c)
+        {
+            if (a>b&& a>c)
+            {
+                return a;
+
+            }
+            else if (b>c && b>c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+
+        }
+    }
+}
